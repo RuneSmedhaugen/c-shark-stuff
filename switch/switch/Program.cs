ba10b80cdda29199dc20f716a58147326fb03130
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-05-13 14:22:30 +0200

    switch

---
+++ switch/switch/Program.cs
@@ -0,0 +1,34 @@
+﻿while (true)
+{
+Console.WriteLine("skriv et tall mellom 1 og 7.");
+var number = Console.ReadLine();
+
+// Lagde en loop istedenfor å lage en funksjon. Fungerer på samme måte men ville gjøre det anderledes enn videoen viste
+    switch (number)
+    {
+        case "1":
+            Console.WriteLine("Mandag");
+            break;
+        case "2":
+            Console.WriteLine("Tirsdag");
+            break;
+        case "3":
+            Console.WriteLine("Onsdag");
+            break;
+        case "4":
+            Console.WriteLine("Torsdag");
+            break;
+        case "5":
+            Console.WriteLine("Fredag");
+            break;
+        case "6":
+            Console.WriteLine("Lørdag");
+            break;
+        case "7":
+            Console.WriteLine("Søndag");
+            break;
+        default:
+            Console.WriteLine("Det er ikke et tall mellom 1 og 7!");
+            break;
+    }
+}
