3b9eb236d05b58fad9f2a5924cee4f643771c87a
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-05-14 14:12:12 +0200

    croc on a rock

---
+++ krokkodillespill/krokkodillespill/Program.cs
@@ -0,0 +1,101 @@
+﻿
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("Hello, what is your name?");
+
+var name = Console.ReadLine();
+Console.WriteLine($"Nice to meet you, {name} and welcome to the crocodile game.");
+Console.WriteLine("how to play: answer with either >, < og =. Write anything else to exit.");
+bool gaming = true;
+
+Random random = new Random();
+int points = 0;
+
+while (gaming)
+{
+
+    int min = 1;
+    int max = 11;
+    int randomNumber = random.Next(min, max + 1);
+    int randomNumber2 = random.Next(min, max + 1);
+    Console.WriteLine($"{randomNumber}_{randomNumber2}");
+    var answer = Console.ReadLine();
+
+    if (randomNumber > randomNumber2)
+    {
+        if (answer == ">")
+        {
+            points++;
+            Console.WriteLine($"Correct! Score: {points}");
+        }
+        else if (answer == "<")
+        {
+            points--;
+            Console.WriteLine($"Wrong! Score: {points} ");
+        }
+        else if (answer == "=")
+        {
+            points--;
+            Console.WriteLine($"Wrong! Score: {points} ");
+        }
+        else
+        {
+            Console.WriteLine("Goodbye");
+            gaming = false;
+
+        }
+
+    }
+    else if (randomNumber < randomNumber2)
+    {
+        if (answer == "<")
+        {
+            points++;
+            Console.WriteLine($"Correct! Score: {points}");
+        }
+        else if (answer == ">")
+        {
+            points--;
+            Console.WriteLine($"Wrong! Score: {points} ");
+        }
+        else if (answer == "=")
+        {
+            points--;
+            Console.WriteLine($"Wrong! Score: {points} ");
+        }
+        else
+        {
+            Console.WriteLine("Goodbye");
+            gaming = false;
+
+        }
+
+    }
+
+    if (randomNumber == randomNumber2)
+    {
+        if (answer == "=")
+        {
+            points++;
+            Console.WriteLine($"Correct! Score: {points}");
+        }
+        else if (answer == "<")
+        {
+            points--;
+            Console.WriteLine($"Wrong! Score: {points} ");
+        }
+        else if (answer == ">")
+        {
+            points--;
+            Console.WriteLine($"Wrong! Score: {points} ");
+        }
+        else
+        {
+            Console.WriteLine("Goodbye");
+            gaming = false;
+        }
+    }
+}
+
+        
---
+++ loops/loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("velg et tall mellom 1 og 3 for å gå videre.");
+var answer = Console.ReadLine();
+
+if (answer == "1")
+{
+    for (int i = 0; i!=5; i++)
+    {Console.WriteLine("Terje er IKKE kul!");}
+    
+}
+else if (answer == "2")
+{
+    string[] tull = new string[] { "dette", "er", "ikke", "gøy", "bare", "tulla", "kekw" };
+    foreach (var arg in tull)
+    {
+        Console.WriteLine(arg);
+    }
+}
+else if (answer == "3")
+{
+    bool condition = true;
+    int rounds = 0;
+
+    while (condition)
+        if (rounds != 10)
+        {
+            Console.WriteLine($"Runde {rounds}");
+            rounds++;
+        }
+        else
+        {
+            Console.WriteLine("Du har nå tatt 10 runder.");
+            condition = false;
+        }
+}
