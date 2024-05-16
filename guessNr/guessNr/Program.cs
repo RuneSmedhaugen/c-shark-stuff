05ddc40e045411885c419e08b5555c8bb89005de
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-05-16 14:07:47 +0200

    guess the number!

---
+++ guessNr/guessNr/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+bool playGame = true;
+
+while (playGame)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(1, 101);
+    Console.WriteLine("Hello! Guess a number between 1 and 100");
+
+    bool guessedCorrect = false;
+
+    while (!guessedCorrect)
+    {
+        var answer = Console.ReadLine();
+
+        if (!int.TryParse(answer, out int guess))
+        {
+            Console.WriteLine("enter a valid number.");
+        }
+
+        if (guess > randomNumber)
+        {
+            Console.WriteLine("Too high!");
+        }
+        else if (guess < randomNumber)
+        {
+            Console.WriteLine("Too low!");
+        }
+        else
+        {
+            Console.WriteLine("Correct!");
+            guessedCorrect = true;
+        }
+    }
+
+    Console.WriteLine("Would you like to play again? (yes/no)");
+    var playAgain = Console.ReadLine();
+
+    if (playAgain == "yes")
+    {
+        guessedCorrect = false;
+    }
+    else
+    {
+        Console.WriteLine("ok asshole.");
+        break;
+
+    }
+}
+
+
