63a11117cda571aa17346a1d8de2904e33b64321
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-05-21 14:55:28 +0200

    klasser og properties

---
+++ klasserOgProperties/klasserOgProperties/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using klasserOgProperties;
+
+Pokemon pikachu = new Pokemon("pikachu", 50, 21);
+Pokemon bulbasaur = new Pokemon("Bulbasaur", 72, 32);
+
+pikachu.ShowInfo();
+bulbasaur.ShowInfo();
---
+++ klasserOgProperties/klasserOgProperties/pokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasserOgProperties
+{
+    internal class Pokemon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Level { get; set; }
+
+        public Pokemon(string name, int health, int level)
+        {
+            Name = name;
+            Health = health;
+            Level = level;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"pokemon: {Name}, health: {Health}, level: {Level}");
+        }
+    }
+}
